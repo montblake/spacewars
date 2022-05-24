c08751887392dbb8e6be203a5ff73d5856536ec7
Blake Montgomery <blakemontgomery312@gmail.com>
2022-05-24 14:27:43 -0500

    Refactor Messaging Module to use Promises

--- modules/Messaging.mjs
+++ modules/Messaging.mjs
@@ -1,42 +1,56 @@
-import { hideButtons } from "./Buttons.mjs";
-
+import {
+	hideButtons
+} from "./Buttons.mjs";
 
 const DELAY = 10;
 
 const warReport = document.querySelector('#warReport');
-const progress = document.querySelector('#progress');
 const messageP = document.querySelector('#message-p');
 
 
 export function showWarReport() {
-    warReport.style.display = "block";
+	warReport.style.display = "block";
 }
 
 export function resetMessage() {
-    message = "";
-    messageP.innerText = "";
-    hideButtons();
+	message = "";
+	messageP.innerText = "";
+	hideButtons();
 }
 
-export function report(message, callback) {
-    let counter = 0;
-    const messageInterval = setInterval(() => {
-        if (message[counter] !== " ") {
-            messageP.innerText += message[counter];
-            counter++
-        } else {
-            messageP.innerText += message.slice(counter, counter + 2)
-            counter += 2
-        }
-        if (counter >= message.length) {
+export function report(message, nextFunction) {
+	if (message.length === 0) return;
+	console.log('report initiated');
+	messages.push(message);
+	controlDeliverySpeed(message)
+		.then(() => {
+			if (nextFunction) {
+				nextFunction();
+			}
+		});
+}
 
-            clearInterval(messageInterval);
-            if (callback) {
-                setTimeout(() => {
-                    callback();
-                }, 500);
-            }
-        }
-    }, DELAY);
-    messages.push(message)
+function controlDeliverySpeed(message) {
+	return new Promise((resolve, reject) => {
+		try {
+			let counter = 0;
+			const letterInterval = setInterval(() => {
+				if (message[counter] !== " ") {
+					messageP.innerText += message[counter];
+					counter++
+				} else {
+					messageP.innerText += message.slice(counter, counter + 2)
+					counter += 2
+				}
+				// messageP.textContent += message[i];
+				// i++;
+				if (counter >= message.length) {
+					clearInterval(letterInterval);
+					resolve('message delivered');
+				}
+			}, DELAY);
+		} catch (error) {
+			reject(`Error: ${error}`);
+		}
+	});
 }
